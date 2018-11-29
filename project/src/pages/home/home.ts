180953f4207b007002ed0f4a44f38f02de92fec7
wnagying <1610010554@qq.com>
2018-11-29 18:56:58 +0800

    wy

--- project/src/pages/home/home.ts
+++ project/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,NavParams } from 'ionic-angular';
+import{ HometailPage} from '../hometail/hometail'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -13,12 +15,67 @@ export class HomePage {
   }
 
   goarticle(){
-    this.navCtrl.push('HometailPage');
+    this.navCtrl.push(HometailPage);
   }
   
-  arr=['推荐','健身','饮食','理疗'];
-  constructor(public navCtrl: NavController) {
+  // arr=['推荐','健身','饮食','理疗'];
 
+
+  bid;
+  tuijian;
+  constructor(public navCtrl: NavController,public http:HttpClient,public params:NavParams) {
+    
+    // console.log('Home参数:',params.data);
+    this.bid = params.data;
+    // this.bid = 3;
+    
+    this.http.post('/api',{
+      "bid":this.bid
+    }).subscribe(data=>{});
+
+    this.http.get('/api').subscribe(data=>{
+      this.tuijian = data;
+      // console.log(data);
+      this.tuijian.forEach(e => {
+        e.imgs = '../assets/imgs/images/'+e.imgs;
+      });
+    });
   }
+
+  jianshen;
+  goJianShen(){    
+    this.http.get('/api/jianshen').subscribe(data=>{
+      this.jianshen = data;
+      // console.log(data);
+      this.jianshen.forEach(e => {
+        e.imgs = '../assets/imgs/images/'+e.imgs;
+      });
+    });
+  }
+
+  yinshi;
+  goYinShi(){    
+    this.http.get('/api/yinshi').subscribe(data=>{
+      this.yinshi = data;
+      // console.log(data);
+      this.yinshi.forEach(e => {
+        e.imgs = '../assets/imgs/images/'+e.imgs;
+      });
+    });
+  }
+
+  liliao;
+  goLiLiao(){    
+    this.http.get('/api/liliao').subscribe(data=>{
+      this.liliao = data;
+      // console.log(data);
+      this.liliao.forEach(e => {
+        e.imgs = '../assets/imgs/images/'+e.imgs;
+      });
+    });
+  }
+
+
+
 
 }
