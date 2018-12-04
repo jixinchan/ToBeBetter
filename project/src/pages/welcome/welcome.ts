1554214438d2b58748b45810d1fc3e8223ba272a
tianyuanfang <1784634905@qq.com>
2018-12-04 20:07:32 +0800

    welcome

---
+++ project/src/pages/welcome/welcome.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, App } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+
+/**
+ * Generated class for the WelcomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html',
+})
+export class WelcomePage {
+  goHome(){
+    this.app.getRootNavs()[0].setRoot(TabsPage);
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public app:App) {
+  }
+
+  ionViewDidLoad() {
+  
+  }
+
+}
