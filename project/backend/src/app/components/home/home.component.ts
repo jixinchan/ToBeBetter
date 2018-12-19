ca3a95486030e003c985159518c73faeafcd6306
wangying1998 <1610010554@qq.com>
2018-12-19 11:13:58 +0800

    个人信息、首页

--- project/backend/src/app/components/home/home.component.ts
+++ project/backend/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  usercount;
+  dcount;
   ngOnInit() {
+    this.http.get('/api/home/usercount').subscribe(data=>{
+      this.usercount = data;
+    });
+    this.http.get('/api/home/dcount').subscribe(data=>{
+      this.dcount = data;
+    });
   }
 
 }
--- project/backend/src/app/components/information/information.component.ts
+++ project/backend/src/app/components/information/information.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-information',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InformationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  userinfo;
   ngOnInit() {
+    this.http.get('/api/userinfo').subscribe(data=>{
+      this.userinfo = data;
+      this.userinfo.forEach(e => {
+        e.birth = moment(e.birth).format("YYYY-MM-DD");
+      });
+    })
   }
+
+  delthis(uid){
+    this.http.post('/api/deluser',{
+      "uid":uid
+    }).subscribe((data)=>{
+      window.location.reload();
+    });
+  }
+
+  
 
 }
