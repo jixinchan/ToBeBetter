3038b3be97793dd1e5ac415aababa811881b1a25
wangying1998 <1610010554@qq.com>
2018-12-24 21:10:27 +0800

    1

--- project/backend/src/app/components/information/information.component.ts
+++ project/backend/src/app/components/information/information.component.ts
@@ -13,12 +13,20 @@ export class InformationComponent implements OnInit {
 
   userinfo;
   ngOnInit() {
-    this.http.get('/api/userinfo').subscribe(data=>{
+      this.http.get('/api/userinfo').subscribe(data=>{
       this.userinfo = data;
       this.userinfo.forEach(e => {
         e.birth = moment(e.birth).format("YYYY-MM-DD");
       });
     })
+  }
+
+  isdel(uid){
+    var a = confirm('要删除这个用户吗？');
+    if(a){
+      this.delthis(uid);
+    }
+    else{}
   }
 
   delthis(uid){
@@ -29,8 +37,27 @@ export class InformationComponent implements OnInit {
     });
   }
 
+  searchresults;
   search(){
-    
+    var key = document.querySelector('#text').value;
+    console.log(key);
+    if(key === ''){
+      alert('请输入搜索关键字');
+    }
+    else{
+      this.http.post('/api/searchusr',{
+        "key":key
+      }).subscribe(data=>{
+        this.searchresults = data;
+        if(document.querySelectorAll('.now')[0].className.indexOf(' hide') == -1){
+          document.querySelectorAll('.now')[0].className += ' hide';
+        }
+      });
+    }
+  }
+
+  reload(){
+    window.location.reload();
   }
 
 }
